d5aa6bae6be2713bdb654839f9b3e2d022457b31
Students <khasperbali@gmail.com>
2017-11-22 14:36:51 -1000

    Work on this

---
+++ apps.js
@@ -0,0 +1,26 @@
+var sumofaliens = 10000;
+var output; //w
+var blue = 3500; //x
+var green = 3500; //y
+var red = 3000; //z
+
+function checklist(w, x, y, z){
+	if (w == 10000) {
+		output = x + y + z;
+}
+ else if (w > 8000) {
+ 	output = y + z;
+ }
+ else if (w < 3000) {
+ 	output = x + z;
+ } 
+  else{
+ 	console.log("MOTHER OF GOD, not the green ones");
+   }
+ {
+ 	console.log("DOOOOOM"); 
+  }
+{
+	console.log("We will not go quietly into the night. We will not vanish without a fight. TODAY WE CELEBRATE OUR INDEPENDANCE DAY!");
+ }
+//checklist(10000)
