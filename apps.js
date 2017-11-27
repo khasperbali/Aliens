d9d20a4ef5f21fcd653bea52b3b141dc76e2da31
Students <khasperbali@gmail.com>
2017-11-27 11:21:35 -1000

    FINAL

--- apps.js
+++ apps.js
@@ -1,26 +1,19 @@
-var sumofaliens = 10000;
-var output; //w
-var blue = 3500; //x
-var green = 3500; //y
-var red = 3000; //z
-
-function checklist(w, x, y, z){
-	if (w == 10000) {
-		output = x + y + z;
+var Blue = 4500;
+var Red = 1000;
+var Green = 4500;
+function aliens(x,y,z){
+    return x + y + z;
+if (z > 5000) {
+     return "MOTHER OF GOD, not the green ones"
+}if (x + z > 8000) {
+     return "Doom"
+}if (y + x < 5000) {
+     return "We will not go quietly into the night. We will not vanish without a fight. TODAY WE CELEBRATE OUR INDEPENDANCE DAY"
 }
- else if (w > 8000) {
- 	output = y + z;
- }
- else if (w < 3000) {
- 	output = x + z;
- } 
-  else{
+ else{
  	console.log("MOTHER OF GOD, not the green ones");
-   }
- {
- 	console.log("DOOOOOM"); 
-  }
-{
-	console.log("We will not go quietly into the night. We will not vanish without a fight. TODAY WE CELEBRATE OUR INDEPENDANCE DAY!");
  }
-//checklist(10000)
+ console.log("Doom");
+}
+console.log("We will not go quietly into the night. We will not vanish without a fight. TODAY WE CELEBRATE OUR INDEPENDANCE DAY")
+}
